cd3f52ced8aab71f0ac5c6ce4ab5f58270bd256f
Sami Lehtiö <sami.lehtio@affecto.com>
2016-04-06 14:28:38 +0300

    Release version 2.0.1

--- SharedFiles/SharedAssemblyInfo.cs
+++ SharedFiles/SharedAssemblyInfo.cs
@@ -6,4 +6,4 @@
 [assembly: AssemblyFileVersion("2.0.1.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("2.0.1-prerelease01")]
+[assembly: AssemblyInformationalVersion("2.0.1")]
